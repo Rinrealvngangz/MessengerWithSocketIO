b857c53b81531559f75c2cfb9b1f5f5c57d230ed
Nguyen Mau Tuan <rinrealvngangz>
2020-11-06 11:22:22 +0700

    add all file js,style,index

---
+++ client/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+   $("#btn_login").click(function(){
+      setTimeout(showAlert,2000,"Successfull!");
+   })
+   function showAlert(mess){
+          alert(mess);
+   }
+})
