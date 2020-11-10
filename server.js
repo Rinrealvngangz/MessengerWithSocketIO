533b1d7d50e25f606b56c7350724dd49285b075c
nguyenMauTuan <rinrealvngangz>
2020-11-10 16:39:44 +0700

    add connect db

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express =require('express');
 const path =require('path');
+const passport =require('passport');
+const mongoose =require('mongoose');
 const port =3000;
 const app =express();
+
+
+mongoose.connect(
+  `mongodb+srv://${nguyenmautuan}:${0949238337}@cluster0.ndtch.mongodb.net/test`
+  ,{ userNewUrlParser:true,useUnifiedTopology: true});
+
+const db =mongoose.connection.then(()=>console.log('db connect success'););
+
+db.on('error',(err)=>console.log(err.message););
+
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
